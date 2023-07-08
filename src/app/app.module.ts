86887bfedde08abf1ef20eacf27b6050114bbe42
Sabrina B <sabrow@hotmail.fr>
2023-07-08 09:11:52 +0200

    chore: create card generic & cardDirective

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardComponent } from './component/card/card.component';
+import {MatCardModule} from '@angular/material/card';
+import { CardDirective } from './directives/card.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]

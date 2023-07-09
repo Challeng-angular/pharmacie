b37d05ae5aebeaa47a3e92c1d84684a48e835bd2
Sabrina B <sabrow@hotmail.fr>
2023-07-09 17:56:08 +0200

    chore: build structure components  app

---
+++ src/app/components/newsletters/newsletters.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newsletters',
+  templateUrl: './newsletters.component.html',
+  styleUrls: ['./newsletters.component.scss']
+})
+export class NewslettersComponent {
+
+}

e36e835c743e4199442b4dca9aaf3baf540254eb
Sabrina B <sabrow@hotmail.fr>
2023-07-10 04:54:30 +0200

    perf: change to pictures array, delete spaces

--- src/app/mock-product/mock-products-list.ts
+++ src/app/mock-product/mock-products-list.ts
@@ -6,45 +6,46 @@ export const PRODUCTS: Product[] = [
     name: "Doliprane",
     note: 2,
     description: "Ce médicament est un antalgique et un antipyrétique qui contient du paracétamol.    Il est utilisé pour faire baisser la fièvre et dans le traitement des affections douloureuses. ",
-    picture1: "../../assets/images/dolipranex3/doliprane1.jpg",
-    picture2: "../../assets/images/dolipranex3/doliprane2.jpg",
-    picture3: "../../assets/images/dolipranex3/doliprane3.jpg"
+    pictures: ["../../assets/images/dolipranex3/doliprane1.jpg",
+      "../../assets/images/dolipranex3/doliprane2.jpg",
+      "../../assets/images/dolipranex3/doliprane3.jpg"]
   },
+
   {id: 2,
     name: "Nurofen",
     note: 4,
     description: "blabla",
-    picture1: "../../assets/images/nurofenx3/nurofen1.jpg",
-    picture2: "../../assets/images/nurofenx3/nurofen1.jpg",
-    picture3: "../../assets/images/nurofenx3/nurofen1.jpg"
+    pictures: ["../../assets/images/nurofenx3/nurofen1.jpg",
+      "../../assets/images/nurofenx3/nurofen1.jpg",
+      "../../assets/images/nurofenx3/nurofen1.jpg"]
   },
+  
   {
     id: 3,
     name: "Ibuprofen",
     note: 2,
     description: "blabla",
-    picture1: "../../assets/images/dolipranex3/doliprane1.jpg",
-    picture2: "../../assets/images/dolipranex3/doliprane1.jpg",
-    picture3: "../../assets/images/dolipranex3/doliprane1.jpg"
-
+    pictures: ["../../assets/images/dolipranex3/doliprane1.jpg",
+      "../../assets/images/dolipranex3/doliprane1.jpg",
+      "../../assets/images/dolipranex3/doliprane1.jpg"]
   },
+
+
   {
     id: 3,
     name: "Spedifen",
     note: 2,
     description: "blabla",
-    picture1: "../../assets/images/dolipranex3/doliprane1.jpg",
-    picture2: "../../assets/images/dolipranex3/doliprane1.jpg",
-    picture3: "../../assets/images/dolipranex3/doliprane1.jpg"
+    pictures: ["../../assets/images/dolipranex3/doliprane1.jpg", "../../assets/images/dolipranex3/doliprane1.jpg", "../../assets/images/dolipranex3/doliprane1.jpg"]
   },
+
 
   {
     id: 4,
     name: "Dafalgan",
     note: 2,
     description: "blabla",
-    picture1: "../../assets/images/dolipranex3/doliprane1.jpg",
-    picture2: "../../assets/images/dolipranex3/doliprane1.jpg",
-    picture3: "../../assets/images/dolipranex3/doliprane1.jpg"
+    pictures: ["../../assets/images/dolipranex3/doliprane1.jpg", "../../assets/images/dolipranex3/doliprane1.jpg", "../../assets/images/dolipranex3/doliprane1.jpg"]
+
   }
 ]
--- src/app/mock-product/product.ts
+++ src/app/mock-product/product.ts
@@ -3,28 +3,25 @@ export class Product {
     name: string;
     note: number;
     description: string;
-  picture1: string;
-  picture2: string;
-  picture3: string;
-  
+  pictures: Array<string>
+
+
   constructor(
     id: number,
     name: string = "Entrer un nom... ",
     note: number = 0,
     description: string = "",
-    picture1: string = "../../assets/images/doliprane1.jpg",
-    picture2: string = "../../assets/images/doliprane2.jpg",
-    picture3: string = "../../assets/images/doliprane3.jpg",
-     
+    pictures: string[] = ["../../assets/images/doliprane1.jpg","../../assets/images/doliprane2.jpg","../../assets/images/doliprane3.jpg"]
+
+
 ) {
     this.id = id;
     this.name = name;
     this.note = note;
     this.description = description;
-    this.picture1 = picture1;
-    this.picture2 = picture2;
-    this.picture3 = picture3;
-    
+    this.pictures = pictures;
+
+
 }
 
 }

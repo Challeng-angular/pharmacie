87155dc7c94441a6a20e2f4d9fa5e4be88109050
Sabrina B <sabrow@hotmail.fr>
2023-07-10 07:56:35 +0200

    feat: add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutUsComponent } from './components/pages/about-us/about-us.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { ListProductsComponent } from './components/pages/list-products/list-products.component';
+import { PromotionsComponent } from './components/pages/promotions/promotions.component';
+import { DetailProductComponent } from './components/pages/detail-product/detail-product.component';
 
 const routes: Routes = [
-  
+
+  { path: 'apropos', component: AboutUsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'accueil', component: HomeComponent },
+  { path: 'produits', component: ListProductsComponent },
+  { path: 'produit/:id', component: DetailProductComponent },
+  { path: 'promotions', component: PromotionsComponent },
+  { path: ' ', redirectTo: 'accueil', pathMatch: 'full' }
 
 ];
 
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
-import { Product } from './product';
-import { PRODUCTS } from './mock-products-list'
+import { Product } from '../mock-product/product';
+import { PRODUCTS } from '../mock-product/mock-products-list'
 
 @Injectable()
 export class ProductsService{
@@ -12,14 +12,14 @@ export class ProductsService{
 
   getProduct(id: number): Product | null{
     let products = this.getProductsList();
-    
+
     for (let index = 0; index < products.length; index++){
       if (id === products[index].id) {
         return products[index];
       }}
-      
+
     return null;
   }
- 
+
 }
 

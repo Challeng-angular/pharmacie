4a7e934716f708be038a4d6380855478dedb371c
Sabrina B <sabrow@hotmail.fr>
2023-07-11 19:54:39 +0200

    refactor: delete starsArray, priceDicount

--- src/app/mock-product/product.ts
+++ src/app/mock-product/product.ts
@@ -1,41 +1,38 @@
 export class Product {
     id: number;
     name: string;
-    starsArray: Array<number>;
     description: string;
     imagesUrl: Array<string>;
-    hasDiscount: boolean;
+    isDiscount: boolean;
     discountPercentage: number;
     price: number;
-    rating: number;
-    discountPrice: number;
+    note: number;
+
 
   constructor(
     id: number,
     name: string = "Entrer un nom... ",
-    starsArray: number[] = [1, 2, 3,4 , 5],
     description: string = "blabla",
     imagesUrl: string[] = ["../../assets/images/dolipranex3/doliprane1.jpg",
       "../../assets/images/dolipranex3/doliprane2.jpg",
       "../../assets/images/dolipranex3/doliprane3.jpg"
     ],
-    hasDiscount: false,
+    isDiscount: false,
     discountPercentage: number,
     price: number,
-    rating: number,
-    discountPrice: number,
+    note: number,
+
   )
   {
     this.id = id;
     this.name = name;
-    this.starsArray = starsArray;
     this.description = description;
     this.imagesUrl = imagesUrl;
-    this.hasDiscount = hasDiscount;
+    this.isDiscount = isDiscount;
     this.discountPercentage = discountPercentage;
     this.price = price;
-    this.rating = rating;
-    this.discountPrice = discountPrice;
+    this.note = note;
+
 }
 
 }

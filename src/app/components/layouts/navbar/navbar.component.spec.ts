6e53aa18c14d1c0d7121321f709173915093cd51
Abdel B <ab.bailoul@gmail.com>
2023-07-11 18:53:41 +0200

    fix: add modules inside navbar spec file

--- src/app/components/layouts/navbar/navbar.component.spec.ts
+++ src/app/components/layouts/navbar/navbar.component.spec.ts
@@ -2,6 +2,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { NavbarComponent } from './navbar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -11,7 +14,13 @@ describe('NavbarComponent', () => {
     TestBed.configureTestingModule({
       declarations: [NavbarComponent],
       providers: [{ provide: ActivatedRoute, useValue: {} }],
-      imports: [MatToolbarModule, RouterLink],
+      imports: [
+        MatToolbarModule,
+        RouterLink,
+        MatIconModule,
+        MatMenuModule,
+        MatButtonModule,
+      ],
     });
     fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;

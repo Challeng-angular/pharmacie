842f98c9749887443bf05cea3f126f6470c2c797
Abdel B <ab.bailoul@gmail.com>
2023-07-13 01:03:09 +0200

    feat: create footer component

--- src/app/components/layouts/footer/footer.component.spec.ts
+++ src/app/components/layouts/footer/footer.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FooterComponent } from './footer.component';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -8,7 +8,8 @@ describe('FooterComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FooterComponent]
+      declarations: [FooterComponent],
+      imports: [MatIconModule],
     });
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;

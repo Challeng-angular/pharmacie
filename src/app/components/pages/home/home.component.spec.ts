ed71e592cd877f6df80ac45b35d75816224d9c31
Abdel B <ab.bailoul@gmail.com>
2023-07-13 21:22:41 +0200

    feat: create bloc-title component + style

--- src/app/components/pages/home/home.component.spec.ts
+++ src/app/components/pages/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { BlocTitleComponent } from '../../bloc-title/bloc-title.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,7 @@ describe('HomeComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [HomeComponent, BlocTitleComponent],
     });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;

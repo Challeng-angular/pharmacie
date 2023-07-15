659389c4a8445f05335567f58dc2cc3d0b8d8dc1
Rémi PONCHE <ponche62880@gmail.com>
2023-07-15 12:49:15 +0200

    refactor: code cleanup

--- src/app/product/detail-product/detail-product.component.ts
+++ src/app/product/detail-product/detail-product.component.ts
@@ -1,21 +1,9 @@
 import { Component } from '@angular/core';
-// import { ProductsService } from '../products.service';
-// import { Product } from '../product';
 
 @Component({
   selector: 'app-detail-product',
-  templateUrl: './detail-product.component.html',
-  styleUrls: ['./detail-product.component.scss']
+  templateUrl: './detail-product.component.html'
 })
 export class DetailProductComponent {
-  // product: Product;
-
-  // constructor(
-    //injection du service
-    // private productsService: ProductsService
-  // ) {}
-  // ngOnInit(): void {
-    
-    // this.productsService.getProduct(id);
-  // }
+ 
 }

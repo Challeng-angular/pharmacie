557a693a32bdc8220f3931e515fc6acdd0901d3d
Sabrina B <sabrow@hotmail.fr>
2023-07-16 13:18:03 +0200

    refactor: add css and img to component contact-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material-module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatCardModule } from '@angular/material/card';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './components/pages/home/home.component';
 import { ContactComponent } from './components/pages/contact/contact.component';
 import { PromotionsComponent } from './components/pages/promotions/promotions.component';
@@ -19,19 +13,18 @@ import { ButtonsComponent } from './components/buttons/buttons.component';
 import { FooterComponent } from './components/layouts/footer/footer.component';
 import { CardDetailsComponent } from './components/card-details/card-details.component';
 import { NewslettersComponent } from './components/newsletters/newsletters.component';
-import { FormComponent } from './components/form/form.component';
 import { NavbarComponent } from './components/layouts/navbar/navbar.component';
 import { AboutUsComponent } from './components/pages/about-us/about-us.component';
 import { ListProductsComponent } from './components/pages/list-products/list-products.component';
 import { CardComponent } from './components/card/card.component';
 import { ProductsService } from './product/products.service';
-import { CguComponent } from './components/pages/cgu/cgu.component';
 import { CgvComponent } from './components/pages/cgv/cgv.component';
 import { BlocTitleComponent } from './components/bloc-title/bloc-title.component';
 import { BlocPresentationComponent } from './components/bloc-presentation/bloc-presentation.component';
 import { ServicesComponent } from './components/pages/services/services.component';
 import { OverviewOrderComponent } from './components/pages/overview-order/overview-order.component';
 import { HeaderComponent } from './components/layouts/header/header.component';
+import { CguComponent } from './components/pages/cgu/cgu.component';
 
 @NgModule({
   declarations: [
@@ -44,7 +37,6 @@ import { HeaderComponent } from './components/layouts/header/header.component';
     FooterComponent,
     CardDetailsComponent,
     NewslettersComponent,
-    FormComponent,
     NavbarComponent,
     AboutUsComponent,
     ListProductsComponent,
@@ -55,18 +47,12 @@ import { HeaderComponent } from './components/layouts/header/header.component';
     BlocPresentationComponent,
     ServicesComponent,
     OverviewOrderComponent,
-    HeaderComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatButtonModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatCardModule,
-    ReactiveFormsModule,
     MaterialModule,
   ],
   providers: [ProductsService],

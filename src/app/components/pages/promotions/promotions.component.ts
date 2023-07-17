34c9cea1254a889cece0d3308d3597a0da5e6fbb
Abdel B <ab.bailoul@gmail.com>
2023-07-17 18:00:13 +0200

    feat: add content for the discount page

--- src/app/components/pages/promotions/promotions.component.ts
+++ src/app/components/pages/promotions/promotions.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-promotions',
   templateUrl: './promotions.component.html',
-  styleUrls: ['./promotions.component.scss']
+  styleUrls: ['./promotions.component.scss'],
+  providers: [ProductsService],
 })
 export class PromotionsComponent {
-
+  isDoctorFirstPict: boolean = true;
+  title: string = 'Nos promotions';
 }

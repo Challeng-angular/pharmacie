38842e23dce467cec723a4061ad5a4c4e03ae617
Rémi PONCHE <ponche62880@gmail.com>
2023-07-19 13:28:33 +0200

    feat: create the service Product for connection API

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,17 +1,60 @@
-import {Injectable} from '@angular/core';
-import { Product } from '../mock-product/product';
-import { PRODUCTS } from '../product/mock-products-list'
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-@Injectable()
-export class ProductsService{
-  productsList: Product[] = PRODUCTS;
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Product } from '../product/product';
 
-  getProductsList(): Product[]{
-    return PRODUCTS
+// ici que je dois modifier
+
+@Injectable({ providedIn: 'root' })
+export class ProductsService {
+  // Attributs
+  private productUrl = 'api/products'; // URL to web api
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  // Methodes
+  constructor(private http: HttpClient) {} //constructeur vide
+
+  /** GET product from the server  */
+  getProducts(): Observable<Product[]> {
+    return this.http
+      .get<Product[]>(this.productUrl)
+      .pipe(tap((_) => console.log('TODO à modifier')));
   }
-  getProductById(id: number): Product | undefined{
-    return this.productsList.find(product => product.id === id);
+
+  /** GET product by id. Will 404 if id not found */
+  getProduct(id: number): Observable<Product> {
+    const url = `${this.productUrl}/${id}`;
+    return this.http.get<Product>(url).pipe(tap((_) => console.log));
   }
 
+
+
+  
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote loggin infrastructure
+      console.error(error); // log to console instead
+
+      return of(result as T); 
+    };
+  }
+
+  /** Log a ProductService message with the Service */
+  private log(message: string) {
+    // TODO: Refactore quand on aura du temps. 
+    console.log(`TODO a supprimer le message, log : ${message}`);
+  }
 }
-

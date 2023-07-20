6923d93d81c7cd818b68a7957e177669279efc22
Abdel B <ab.bailoul@gmail.com>
2023-07-20 14:07:14 +0200

    feat: add bg header + catgories anchors links

--- src/app/components/layouts/header/header.component.ts
+++ src/app/components/layouts/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,18 +11,21 @@ import { FormControl } from '@angular/forms';
 })
 export class HeaderComponent {
   menuHeaderItems: any[] = [
-    { label: 'Compléments alimentaires' },
-    { label: 'Spécialités en vente libre' },
-    { label: 'Cosmétiques & Maquillages' },
-    { label: 'Phytothérapie / Homéopathie' },
-    { label: 'Spagyrie' },
+    { label: 'Compléments alimentaires', fragment: 'section-1' },
+    { label: 'Spécialités en vente libre', fragment: 'section-2' },
+    { label: 'Cosmétiques & Maquillages', fragment: 'section-3' },
+    { label: 'Phytothérapie / Homéopathie', fragment: 'section-4' },
+    { label: 'Spagyrie', fragment: 'section-5' },
   ];
 
   disableSelect = new FormControl(false);
   private breakpointSubscription: Subscription;
   public isSmallScreen = false;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.breakpointSubscription = this.breakpointObserver
@@ -35,5 +39,10 @@ export class HeaderComponent {
     if (this.breakpointSubscription) {
       this.breakpointSubscription.unsubscribe();
     }
+  }
+
+  handleClick(frag: string) {
+    console.log(frag);
+    this.router.navigateByUrl('produits#' + frag);
   }
 }
